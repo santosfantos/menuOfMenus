123d2474d128cab5740ce10a2dda57dfba8ebc42
dotan <dotan@netapp.com>
2021-06-20 23:09:47 +0300

    null fix

--- src/components/Menu/SubMenu.tsx
+++ src/components/Menu/SubMenu.tsx
@@ -13,6 +13,9 @@ const SubMenu: FC<{ depthLevel: number; items: Array<Item>; }> = (props) => {
     return (
         <ul className={`${classes.SubMenu} ${props.depthLevel === 0 ? classes.RootSubMenu : ''}`}>
             {props.items.map((item, itemIndex) => {
+                if(item == null) {
+                    return;
+                }
                 const itemKey    = `item-${props.depthLevel}-${itemIndex}-${Date.now().toString()}`;
                 const hasSubMenu = isEmpty(item.children) === true ? false : true;
 
